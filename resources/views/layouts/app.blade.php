553bfaa6dfd5bda359377aaeabf94d3c10f0c0bf
Anuradhapps <anuradha.pps2024@gmail.com>
2024-04-04 14:52:30 +0530

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a href="{{route('home')}}">New Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('post.all')}}">All Posts</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ Route::get('/home',[HomeController::class,'index'])->name('home');
 //post
 Route::post('/post/store',[postController::class,'store'])->name('post.store');
 Route::get('/post/{postId}/show',[postController::class,'show'])->name('post.show');
+Route::get('/post/all-post',[HomeController::class,'allPost'])->name('post.all');
